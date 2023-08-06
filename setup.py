63497bb10f6671cd07b309585f862894bd2bb392
Rémy Tuyéras <36420536+remytuyeras@users.noreply.github.com>
2023-08-06 12:27:34 -0400

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,22 +1,29 @@
 from distutils.core import setup
+import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
   name = 'HaploDynamics',
-  packages = ['HaploDynamics'],
+  packages=setuptools.find_packages(),
   version = 'v0.1-beta.0',
   license='gpl-3.0',
-  description = 'A python library to develop genomic data simulators',
   author = 'Remy Tuyeras',
   author_email = 'rtuyeras@gmail.com',
+  description = 'A python library to develop genomic data simulators',
+  long_description = long_description,
+  long_description_content_type = "text/markdown",
   url = 'https://github.com/remytuyeras/HaploDynamics',
   download_url = 'https://github.com/remytuyeras/HaploDynamics/archive/refs/tags/v0.1-beta.0.tar.gz',
   keywords = ['Simulator', 'Genomics', 'Genomic', 'Microarray','SNP chip','VCF', 'Linkage disequilibrium', 'Hardy-Weinberg equilibrium'],
   install_requires=['scipy'],
   classifiers=[
-    'Development Status :: 4 - Beta',      #"3 - Alpha", "4 - Beta" or "5 - Production/Stable"
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Development Status :: 4 - Beta',  #"3 - Alpha", "4 - Beta" or "5 - Production/Stable"
+    'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: GNU General Public License v3.0',   # Again, pick a license
+    'License :: OSI Approved :: GNU General Public License v3.0',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.9',
   ],
