236bc67f153f811baa696434b912bdd63f62073e
Rémy Tuyéras <36420536+remytuyeras@users.noreply.github.com>
2023-08-06 22:22:47 -0400

    Add files via upload

--- setup.py
+++ setup.py
@@ -3,13 +3,10 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-print(setuptools.find_packages(include=['HaploDynamics','HaploDynamics.HaploDX','HaploDynamics.Framework']))
-exit()
-
 setuptools.setup(
   name = 'HaploDynamics',
   packages=setuptools.find_packages(include=['HaploDynamics','HaploDynamics.HaploDX','HaploDynamics.Framework']),
-  version = 'v0.1-beta.8',
+  version = 'v0.1-beta.9',
   license='gpl-3.0',
   author = 'Remy Tuyeras',
   author_email = 'rtuyeras@gmail.com',
@@ -17,7 +14,7 @@ setuptools.setup(
   long_description = long_description,
   long_description_content_type = "text/markdown",
   url = 'https://github.com/remytuyeras/HaploDynamics',
-  download_url = 'https://github.com/remytuyeras/HaploDynamics/archive/refs/tags/v0.1-beta.8.tar.gz',
+  download_url = 'https://github.com/remytuyeras/HaploDynamics/archive/refs/tags/v0.1-beta.9.tar.gz',
   keywords = ['Simulator', 'Genomics', 'Genomic', 'Microarray','SNP chip','VCF', 'Linkage disequilibrium', 'Hardy-Weinberg equilibrium'],
   install_requires=['scipy'],
   classifiers=[
