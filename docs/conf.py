f94db77c096e1290ddb2165a817e055b4ec866bb
Rémy Tuyéras <36420536+remytuyeras@users.noreply.github.com>
2023-08-09 13:03:37 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -2,35 +2,10 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'HaploDynamics'
+copyright = '2023, Remy Tuyeras'
+author = 'Remy Tuyeras'
 
-release = '0.1'
-version = '0.1.0'
+release = '0.2-beta.4'
+version = '0.2-beta.4'
 
-# -- General configuration
-
-extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
-    'sphinx_rtd_theme',
-]
-
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
-}
-intersphinx_disabled_domains = ['std']
-
-templates_path = ['_templates']
-
-# -- Options for HTML output
-
-html_theme = 'sphinx_rtd_theme'
-
-# -- Options for EPUB output
-epub_show_urls = 'footnote'
